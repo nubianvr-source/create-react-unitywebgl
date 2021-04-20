f4402af908f5f2891e090e6f43937ef1428a9e1a
Andy <services.nubianvr@gmail.com>
2021-04-20 14:11:39 +0000

    Unity webgl push

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+//import logo from './logo.svg';
+import './App.css';
+import Unity, { UnityContext } from 'react-unity-webgl'
+import {useState, useEffect} from 'react'
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
 
-  handleClick = api => e => {
-    e.preventDefault()
+function App() {
+  const [progression, setProgression] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+  //const [didError, setDidError] = useState(false)
+  //const [errorMessage, setErrorMessage] = useState("")
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
 
-  render() {
-    const { loading, msg } = this.state
+  const unityContext = new UnityContext({
+    loaderUrl: "Build/Build.loader.js",
+    dataUrl: "Build/Build.data",
+    frameworkUrl: "Build/Build.framework.js",
+    codeUrl: "Build/Build.wasm",
+  });
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+  unityContext.on('progress', (progression) => {
+    setProgression(progression)
+    
+  })
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+  unityContext.on('loaded', () => {
+    setIsLoading(false)
+    console.log(progression)
+  })
+  
+ 
+  return (
+      <div className='App'>
+      <p>{`Loading... ${progression * 100}%`}</p>
+      <Unity style={{height: "100vh", width: "100%"}} unityContext={unityContext}/>
       </div>
-    )
-  }
+      
+  );
 }
 
-export default App
+export default App;
